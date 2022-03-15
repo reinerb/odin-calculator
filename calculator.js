eef62c3f12a10098cc8766276595d69a5ebd6744
Ben Reiner <benjamin.t.reiner@gmail.com>
2022-03-14 22:46:48 -0400

    Add functions for operators

---
+++ calculator.js
@@ -0,0 +1,16 @@
+// Operates a . b
+const add = function (a, b) {
+  return parseInt(a) + parseInt(b);
+};
+
+const subtract = function (a, b) {
+  return parseInt(a) - parseInt(b);
+};
+
+const multiply = function (a, b) {
+  return parseInt(a) * parseInt(b);
+};
+
+const divide = function (a, b) {
+  return b == 0 ? "ERROR" : parseInt(a) / parseInt(b);
+};
